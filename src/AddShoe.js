497d74d0c8861acaea57115b3e6c78917bf0f5ab
jacobbeattie <jacobanarubeattie@gmail.com>
2023-07-29 12:09:43 +1200

    Updated popup to let user know if shoe already added

--- src/AddShoe.js
+++ src/AddShoe.js
@@ -6,17 +6,19 @@ import ShoeAddedPopup from './ShoeAddedPopup';
 const AddShoe = () => {
   const [selectedShoe, setSelectedShoe] = useState('');
   const [showPopup, setShowPopup] = useState(false);
+  const [alreadyAdded, setAlreadyAdded] = useState(false);
+  
 
   // List of shoes available in the dropdown with their respective data
   const shoeOptions = [
-    { name: 'Shoe 1', price: '$100', releaseDate: '2023-07-25', image: 'path/to/shoe-image-1.jpg' },
-    { name: 'Shoe 2', price: '$120', releaseDate: '2023-08-15', image: 'path/to/shoe-image-2.jpg' },
-    { name: 'Shoe 3', price: '$90', releaseDate: '2023-09-01', image: 'path/to/shoe-image-3.jpg' },
-    { name: 'Shoe 4', price: '$80', releaseDate: '2023-09-15', image: 'path/to/shoe-image-4.jpg' },
+    { name: 'Nike Air Jordan 1 Low Shadow 3.0', price: '$380', releaseDate: 'July 2021', image: 'path/to/shoe-image-1.jpg' },
+    { name: 'Adidas Yeezy 350 V2 Citrin', price: '$380', releaseDate: 'September 2020', image: 'path/to/shoe-image-2.jpg' },
+    { name: 'Nike Air VaporMax Flyknit 3', price: '$310', releaseDate: 'March 2021', image: 'path/to/shoe-image-3.jpg' },
+    { name: 'Adidas NMD R1 Japan Triple White', price: '$280', releaseDate: 'August 2017', image: 'path/to/shoe-image-4.jpg' },
   ];
 
   // Get the dispatch function from the ShoeContext
-  const { dispatch } = useContext(ShoeContext);
+  const { state, dispatch } = useContext(ShoeContext);
 
   // Function to handle the selection of a shoe
   const handleShoeSelection = (event) => {
@@ -27,13 +29,18 @@ const AddShoe = () => {
   const handleAddShoeToDashboard = () => {
     // Check if a shoe is selected
     if (selectedShoe) {
-      // Find the selected shoe object from the shoeOptions array
-      const selectedShoeObject = shoeOptions.find((shoe) => shoe.name === selectedShoe);
-
-      if (selectedShoeObject) {
-        // Dispatch the action to add the selected shoe object to the context state
-        dispatch({ type: 'ADD_SHOE', payload: selectedShoeObject });
-        setShowPopup(true); // Show the pop-up on successful addition
+      // Check if the selected shoe is already on the dashboard
+      const isAlreadyAdded = state.shoes.some((shoe) => shoe.name === selectedShoe);
+      if (isAlreadyAdded) {
+        setAlreadyAdded(true); // Set the alreadyAdded state to true if the shoe is already added
+        setShowPopup(true); // Show the pop-up
+      } else {
+        const selectedShoeObject = shoeOptions.find((shoe) => shoe.name === selectedShoe);
+        if (selectedShoeObject) {
+          dispatch({ type: 'ADD_SHOE', payload: selectedShoeObject });
+          setShowPopup(true); // Show the pop-up for successful addition
+          setAlreadyAdded(false); // Reset the alreadyAdded state
+        }
       }
     }
   };
@@ -74,7 +81,13 @@ const AddShoe = () => {
           <button onClick={handleAddShoeToDashboard}>Add to Dashboard</button>
 
           {/* Conditionally render the pop-up based on showPopup state */}
-          {showPopup && <ShoeAddedPopup onClose={closePopup} />}
+          {showPopup && (
+            <ShoeAddedPopup
+              onClose={closePopup}
+              alreadyAdded={alreadyAdded}
+              selectedShoe={selectedShoe}
+            />
+          )}
         </div>
       </main>
       <footer>
--- src/ShoeAddedPopup.js
+++ src/ShoeAddedPopup.js
@@ -1,12 +1,23 @@
+// ShoeAddedPopup.js
+
 import React from 'react';
 
-const ShoeAddedPopup = ({ onClose }) => {
+const ShoeAddedPopup = ({ onClose, alreadyAdded, selectedShoe }) => {
   return (
     <div className="popup-overlay">
       <div className="popup">
         <div className="popup-content">
-          <h3>Shoe Added Successfully</h3>
-          <p>Your selected shoe has been added to the dashboard.</p>
+          {alreadyAdded ? (
+            <div>
+              <h3>Shoe Already Added</h3>
+              <p>The shoe "{selectedShoe}" is already on the dashboard.</p>
+            </div>
+          ) : (
+            <div>
+              <h3>Shoe Added Successfully</h3>
+              <p>Your selected shoe "{selectedShoe}" has been added to the dashboard.</p>
+            </div>
+          )}
           <button onClick={onClose}>Close</button>
         </div>
       </div>
@@ -15,3 +26,4 @@ const ShoeAddedPopup = ({ onClose }) => {
 };
 
 export default ShoeAddedPopup;
+
