98c5b17932299031e8c8c8c1abb74f621d26cc28
jacobbeattie <jacobanarubeattie@gmail.com>
2023-08-04 16:54:20 +1200

    Added a new create account panel on the login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
+import CreateAccount from './CreateAccount';
 
 function LoginPage() {
   const navigateTo = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(false); // Add the error state
+  const [error, setError] = useState(false);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
-    setError(false); // Reset the error state when the user starts typing
+    setError(false);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    setError(false); // Reset the error state when the user starts typing
+    setError(false);
   };
 
   const handleLogin = (e) => {
     e.preventDefault();
     if (username === 'Testing123' && password === 'Testing123') {
-      // Redirect to the dashboard page on successful login
       navigateTo('/dashboard');
     } else {
-      // Set the error state to true if login is unsuccessful
       setError(true);
     }
+  };
+
+  const handleToggleCreateAccount = () => {
+    setShowCreateAccount(!showCreateAccount);
+  };
+
+  const handleToggleLogin = () => {
+    setShowCreateAccount(!showCreateAccount);
   };
 
   return (
@@ -37,41 +45,49 @@ function LoginPage() {
         </div>
       </header>
       <main className="login-main">
-        <div className="login-container">
-          <h2 className="login-logo">Login</h2>
-          <form id="login-form" onSubmit={handleLogin}>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="login-input"
-              required
-              value={username}
-              onInput={handleUsernameChange}
-              placeholder="Username"
-            />
+        {showCreateAccount ? (
+          // Render the create account box
+          <CreateAccount onToggleLogin={handleToggleLogin} />
+        ) : (
+          // Render the login panel
+          <div className="login-container">
+            <h2 className="login-logo">Login</h2>
+            <form id="login-form" onSubmit={handleLogin}>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="login-input"
+                required
+                value={username}
+                onInput={handleUsernameChange}
+                placeholder="Username"
+              />
 
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="login-input"
-              required
-              value={password}
-              onInput={handlePasswordChange}
-              placeholder="Password"
-            />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="login-input"
+                required
+                value={password}
+                onInput={handlePasswordChange}
+                placeholder="Password"
+              />
 
-            <button type="submit" className="login-button">
-              Login
-            </button>
-          </form>
-        </div>
+              <button type="submit" className="login-button">
+                Login
+              </button>
+              <p className='no-account' onClick={handleToggleCreateAccount}>
+                Don't have an account?
+              </p>
+            </form>
 
-        {/* Conditionally render the dialog */}
-        {error && (
-          <div className="error-dialog">
-            <p>Incorrect username or password. Please try again.</p>
+            {error && (
+              <div className="error-dialog">
+                <p>Incorrect username or password. Please try again.</p>
+              </div>
+            )}
           </div>
         )}
       </main>
